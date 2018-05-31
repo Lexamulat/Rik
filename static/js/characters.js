9a03eb794bac62696450b112baa433d8edb90ca5
lexamulat <lexamulat@mail.ru>
2018-05-31 19:04:42 +0300

    have alghoritm with px/ start work at %ts

---
+++ static/js/characters.js
@@ -0,0 +1,128 @@
+async function GetCharactersInfo(callback) {
+
+    //!! Take Get Params eraise @?uid=3@ to 3
+    var QueryStr = (document.location.search).substr(10)
+    GetCharactersData = await Request("https://rickandmortyapi.com/api/character/" + QueryStr)
+    console.log(GetCharactersData)
+    callback()
+}
+
+function CanIDisplayIt(SideOfASquareInPix, ClientWidthInPix, ClientHeightInPix, NumOfSquares) {
+    var XInsertPosition = 0,
+        YInsertPosition = 0,
+        j = 0;
+    for (var i = 0; i < NumOfSquares; i++) {
+        XInsertPosition = j * SideOfASquareInPix
+        j++;
+        if ((XInsertPosition + SideOfASquareInPix) >= ClientWidthInPix) {
+            j = 1;
+            YInsertPosition += SideOfASquareInPix;
+            XInsertPosition = 0;
+        }
+
+        // console.log(XInsertPosition, YInsertPosition)
+        if (YInsertPosition + SideOfASquareInPix >= ClientHeightInPix) {
+            return 0;
+        }
+    }
+
+    return 1;
+
+
+}
+
+function DisplayCharacters(SideOfASquareInPix, ClientWidthInPix, NumOfSquares) {
+    console.log("in display func")
+
+
+    var XInsertPosition = 0,
+        YInsertPosition = 0,
+        j = 0;
+    for (var i = 0; i < NumOfSquares; i++) {
+
+        XInsertPosition = j * SideOfASquareInPix
+        j++;
+        if ((XInsertPosition + SideOfASquareInPix) >= ClientWidthInPix) {
+            j = 1;
+            YInsertPosition += SideOfASquareInPix
+            XInsertPosition = 0;
+        }
+
+
+        var t = newElem('image', {
+            x: XInsertPosition + 'px',
+            y: YInsertPosition + 'px',
+            width: SideOfASquareInPix,
+            height: SideOfASquareInPix,
+        })
+
+        one.appendChild(t);
+
+    }
+}
+
+function CountBlocksSize() {
+    console.log(GetCharactersData.length);
+    var ClientWidthInPix = Number(document.documentElement.clientWidth),
+        ClientHeightInPix = Number(document.documentElement.clientHeight),
+        ScreenArea = ClientWidthInPix * ClientHeightInPix,
+        // !! START square size = 10 px
+        SideOfASquareInPix = 10,
+        AreaOfOneSquare = SideOfASquareInPix * SideOfASquareInPix,
+        //!! RESERVED first square for @back@
+        NumOfSquares = Number(GetCharactersData.length) + 1;
+
+    // !!DIVIDED the algorithm into 2 parts. For optimize the search for the optimal length of the square side
+    while ((NumOfSquares * AreaOfOneSquare) < ScreenArea) {
+
+        SideOfASquareInPix = SideOfASquareInPix * 2;
+        AreaOfOneSquare = SideOfASquareInPix * SideOfASquareInPix;
+        // console.log("first")
+    }
+
+    SideOfASquareInPix = SideOfASquareInPix / 2;
+    AreaOfOneSquare = SideOfASquareInPix * SideOfASquareInPix;
+
+    while ((NumOfSquares * AreaOfOneSquare) < ScreenArea) {
+        SideOfASquareInPix++;
+        AreaOfOneSquare = SideOfASquareInPix * SideOfASquareInPix;
+        // console.log("second")
+    }
+    SideOfASquareInPix--;
+    AreaOfOneSquare = SideOfASquareInPix * SideOfASquareInPix;
+
+    console.log("screen", ScreenArea)
+    console.log("num", NumOfSquares)
+    console.log("side size", SideOfASquareInPix)
+    console.log("s of squares", NumOfSquares * (SideOfASquareInPix * SideOfASquareInPix))
+
+    // CanIDisplayIt(SideOfASquareInPix, ClientWidthInPix, ClientHeightInPix, NumOfSquares)
+    var can = CanIDisplayIt(SideOfASquareInPix, ClientWidthInPix, ClientHeightInPix, NumOfSquares)
+    console.log("can", can)
+
+    while (can == 0) {
+        SideOfASquareInPix--;
+        can = CanIDisplayIt(SideOfASquareInPix, ClientWidthInPix, ClientHeightInPix, NumOfSquares)
+        console.log("can", can)
+    }
+    console.log(SideOfASquareInPix)
+
+    DisplayCharacters(SideOfASquareInPix, ClientWidthInPix, NumOfSquares)
+}
+
+
+
+var GetCharactersData = {}
+
+async function start() {
+
+    console.log("Characters start")
+
+
+    GetCharactersInfo(function() {
+        CountBlocksSize()
+    });
+}
+
+
+window.onload = start;
