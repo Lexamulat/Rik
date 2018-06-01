530af37c02db040579d18e50148ec7bc331bddd2
lexamulat <lexamulat@mail.ru>
2018-06-01 11:13:50 +0300

    test classes

--- static/js/test.js
+++ static/js/test.js
@@ -1,5 +1,9 @@
 function ContentClass() {
     var content = {};
+    //GENERAL METHODS
+    this.someParentMethd = function() {
+        console.log("someParentMethd")
+    };
 
     this.popUp = function() {
         console.log("popUp")
@@ -8,46 +12,63 @@ function ContentClass() {
     this.popDown = function() {
         console.log("popDown")
     };
+
     this.setContent = function(inputContent) {
         content = inputContent
     };
-    this.getContent = function(inputContent) {
+
+    this.getContent = function() {
         return content;
-
     };
-    this.showContent = function(inputContent) {
-        console.log("content: ", content)
 
+    this.showContent = function() {
+        console.log("content: ", content)
+    };
+    //REDEFINED METHODS
+
+    this.displayContent = function() {
+        console.log("parent disp")
     };
 }
 
 
 function PlanetsContentClass() {
     ContentClass.call(this); // отнаследовать
+    this.displayContent = function() {
+        console.log("planets disp")
+    };
 }
 
 function CharactersContentClass() {
     ContentClass.call(this); // отнаследовать
+    this.displayContent = function() {
+        console.log("characters disp")
+        this.someParentMethd()
+    };
 }
 
 function start() {
 
     var Planets = new PlanetsContentClass();
     var Characters = new CharactersContentClass();
-    console.log("plan cont")
-    Planets.setContent('plan');
-    Planets.showContent();
+    Planets.displayContent();
+    Characters.displayContent();
 
-    console.log("char cont")
 
-    Characters.setContent('char');
-    Characters.showContent();
+    // console.log("plan cont")
+    // Planets.setContent('plan');
+    // Planets.showContent();
 
-    console.log("plan cont")
+    // console.log("char cont")
 
-    Planets.showContent();
-    Planets.popUp();
-    Characters.popUp();
+    // Characters.setContent('char');
+    // Characters.showContent();
+
+    // console.log("plan cont")
+
+    // Planets.showContent();
+    // Planets.popUp();
+    // Characters.popUp();
     // console.log(Planets.getContent())
     // Planets.showContent();
 
